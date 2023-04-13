6c46338e66e625ab45a426ed66e96770324251e6
bendafit <benidanuarifitrio@gmail.com>
2023-04-14 00:07:07 +0700

    first commit

--- scripts/CP.js
+++ scripts/CP.js
@@ -8,5 +8,17 @@ function cpPage() {
       <p class="card-text">Di akhir fase E, peserta didik dapat menentukan perbandingan trigonometri dan memecahkan masalah yang melibatkan sigitiga siku - siku.</p>
     </div>
   </div>
+  <div class="card mt-2">
+    <div class="card-header">
+     Capaian Pembelajaran
+    </div>
+    <div class="card-body">
+      <p class="card-text"><ol class="list-group list-group-numbered">
+      <li class="list-group-item">Peserta didik mampu menentukan dengan teliti panjang sisi sebuah segitiga siku – siku dan besar sudut salah satu segitiga siku- siku secara mandiri dengan benar</li>
+      <li class="list-group-item">Peserta didik mampu menentukan dengan teliti nilai perbandingan trigonometri pada segitiga siku – siku jika satu nilai perbandingan trigonometri yang lainnya diketahui secara mandiri dengan benar</li>
+      <li class="list-group-item">Peserta didik mampu menghitung dengan teliti nilai perbandingan trigonometri pada sudut – sudut istimewa secara mandiri dengan benar</li>
+    </ol></p>
+    </div>
+  </div>
     `
 }
