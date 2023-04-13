efbd52f833161375ea5acdfb25d9dd9001056bc4
bendafit <benidanuarifitrio@gmail.com>
2023-04-13 22:28:39 +0700

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -13,3 +13,32 @@ const sidebarBtn = document.querySelector(".bx-menu");
 sidebarBtn.addEventListener("click", () => {
     sidebar.classList.toggle("close");
 });
+
+// $(document).ready(function() {
+//     var trigger = $('.hamburger'),
+//         overlay = $('.overlay'),
+//         isClosed = false;
+
+//     trigger.click(function() {
+//         hamburger_cross();
+//     });
+
+//     function hamburger_cross() {
+
+//         if (isClosed == true) {
+//             overlay.hide();
+//             trigger.removeClass('is-open');
+//             trigger.addClass('is-closed');
+//             isClosed = false;
+//         } else {
+//             overlay.show();
+//             trigger.removeClass('is-closed');
+//             trigger.addClass('is-open');
+//             isClosed = true;
+//         }
+//     }
+
+//     $('[data-toggle="offcanvas"]').click(function() {
+//         $('#wrapper').toggleClass('toggled');
+//     });
+// });
