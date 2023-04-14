d02525651ea21d8293c9f97265a7b68a0e47ec57
bendafit <benidanuarifitrio@gmail.com>
2023-04-14 17:05:33 +0700

    first commit

---
+++ scripts/bio.js
@@ -0,0 +1,5 @@
+function bioPage() {
+    judul.innerText = `Biografi Penulis`
+    content.innerHTML = `
+<img src="bio.png" class="img-fluid mt-3" alt="...">`
+}
---
+++ scripts/peta.js
@@ -0,0 +1,5 @@
+function petaPage() {
+    judul.innerText = `Peta Konsep Trigonometri`
+    content.innerHTML = `
+ <img src="peta.png" class="img-fluid mt-3" alt="...">`
+}
