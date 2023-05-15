b859b9b778be5d5b45cc85d365231141ea30fded
bendafit <benidanuarifitrio@gmail.com>
2023-05-16 06:28:04 +0700

    first commit

--- scripts/CP.js
+++ scripts/CP.js
@@ -1,6 +1,5 @@
 function cpPage() {
     judul.innerText = `CP & Tujuan Pembelajaran`
-    content.innerHTML = `<div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-    <img src="KOMPETENSI.png" class="img-fluid mt-3" alt="...">
-    </div>`
+    content.innerHTML = `
+    <img src="KOMPETENSI.png" class="img-fluid mt-3" alt="...">`
 }
