8aed461a1973d21ff547a9b7664bee2da2db375d
bendafit <benidanuarifitrio@gmail.com>
2023-05-16 06:24:31 +0700

    first commit

--- scripts/CP.js
+++ scripts/CP.js
@@ -1,24 +1,6 @@
 function cpPage() {
     judul.innerText = `CP & Tujuan Pembelajaran`
     content.innerHTML = `<div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-    <div class="card-header">
-     Capaian Pembelajaran
-    </div>
-    <div class="card-body">
-      <p class="card-text">Di akhir fase E, peserta didik dapat menentukan perbandingan trigonometri dan memecahkan masalah yang melibatkan sigitiga siku - siku.</p>
-    </div>
-  </div>
-  <div class="card mt-2 shadow p-3 mb-5 bg-body-tertiary rounded">
-    <div class="card-header">
-     Tujuan Pembelajaran
-    </div>
-    <div class="card-body">
-      <p class="card-text"><ol class="list-group list-group-numbered">
-      <li class="list-group-item">Peserta didik mampu menentukan dengan teliti panjang sisi sebuah segitiga siku – siku dan besar sudut salah satu segitiga siku- siku secara mandiri dengan benar</li>
-      <li class="list-group-item">Peserta didik mampu menentukan dengan teliti nilai perbandingan trigonometri pada segitiga siku – siku jika satu nilai perbandingan trigonometri yang lainnya diketahui secara mandiri dengan benar</li>
-      <li class="list-group-item">Peserta didik mampu menghitung dengan teliti nilai perbandingan trigonometri pada sudut – sudut istimewa secara mandiri dengan benar</li>
-    </ol></p>
-    </div>
-  </div>
-    `
+    <img src="KOMPETENSI.png" class="img-fluid mt-3" alt="...">
+    </div>`
 }
