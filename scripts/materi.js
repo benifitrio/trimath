bb75eccd098e4d8224221360b40da4ab74bde33a
bendafit <benidanuarifitrio@gmail.com>
2023-05-16 23:17:59 +0700

    first commit

--- scripts/materi.js
+++ scripts/materi.js
@@ -32,8 +32,33 @@ function materiPage() {
   <tbody>
     <tr>
       <td><i>Stimulation</i>
-      (Pemberian Rangsangan )</td>
+      (Pemberian Rangsangan)</td>
       <td>Peserta didik diberikan permasalahan diawal dengan mengamati salah satu pertanyaan pada materi trigonometri yang disajikan  di dalam e-modul.</td>
+    </tr>
+    <tr>
+      <td><i>Problem Statement</i>
+      (Pernyataan/Identifikasi masalah)</td>
+      <td>Peserta didik dapat menuliskan apa saja pertanyaan untuk mengemukakan ide dalam identifikasi masalah dari hasil pengamatan untuk menyelesaikan masalah pada soal tersebut.</td>
+    </tr>
+    <tr>
+      <td><i>Data Collection</i>
+      (Pengumpulan Data)</td>
+      <td>Peserta didik diberikan kesempatan untuk menyelesaikan masalah berdasarkan identifikasi masalah.</td>
+    </tr>
+    <tr>
+      <td><i>Data Processing</i>
+      (Pengolahan Data)</td>
+      <td>Peserta didik dapat menuliskan proses pengolahan data dari hasil pengumpulan data untuk menyelesaikan masalah.</td>
+    </tr>
+    <tr>
+      <td><i>Verification</i>
+      (Pembuktian)</td>
+      <td>Peserta didik melakukan verifikasi dan pemeriksaan  hasil dari pengolahan data apakah sudah sesuai dengan pertanyaan yang ditunjukkan.</td>
+    </tr>
+    <tr>
+      <td><i>Generalization</i>
+      (Menarik Kesimpulan/Generalisasi)</td>
+      <td>Peserta didik dapat menyajikan hasil pekerjaannya berdasarkan pembuktian yang di dapat dari uraian masalah tersebut.</td>
     </tr>
   </tbody>
 </table>
