92848fb6f89c38731f00bf51ba60b557b086da22
bendafit <benidanuarifitrio@gmail.com>
2023-05-16 22:57:13 +0700

    first commit

--- scripts/materi.js
+++ scripts/materi.js
@@ -3,19 +3,18 @@ function materiPage() {
     content.innerHTML = `
     <div class="section_our_solution">
     <div class="row">
-      <div class=" col col-lg-12 col-md-12 col-sm-6">
+      <div class=" col">
         <div class="our_solution_category">
           <div class="solution_cards_box">
             <div class="solution_card">
               <div class="hover_color_bubble"></div>
               <div class="solu_title">
-                <h3>Identitas Trigonometri</h3>
+                <h3>Deskripsi E-Modul</h3>
               </div>
               <div class="solu_description">
                 <p>
                 Identitas trigonometri dikenal istilah sinus, cosinus, dan tangen....
                 </p>
-                <button type="button" class="read_more_btn">Read More</button>
               </div>
             </div>
           </div>
