804da896ddb5c569e52246265cfc109486f03656
bendafit <benidanuarifitrio@gmail.com>
2023-05-16 23:13:48 +0700

    first commit

--- scripts/materi.js
+++ scripts/materi.js
@@ -12,9 +12,31 @@ function materiPage() {
                 <h3>Deskripsi E-Modul</h3>
               </div>
               <div class="solu_description">
-                <p>
-                Identitas trigonometri dikenal istilah sinus, cosinus, dan tangen....
-                </p>
+                <p style="text-indent:2rem">
+                Keberhasilan anda dalam mempelajari materi  Trigonometri di dalam E-Modul ini bergantung pada ketekunan dan kedisiplinan peserta didik dan mematuhi langkah belajar yang ada. Belajar denan E=Modul ini dapat dilakukan secara mandiri maupun berkelompok baik di sekolah maupun diluar sekolah.
+  </p>
+  <p style="text-indent:2rem">
+	E-Modul ini disusun dan disesuaikan dengan model pembelajaran Discovery Learning model pembelajaran ini dipilih karena diharapkan peserta didik dapat mengembangkan belajar peserta didik aktif dengan menemukan sendiri dan menyelidiki sendiri suatu konsep dari pembelajaran.
+	“Model Discovery Learning adalah suatu model pembelajaran untuk mengembangkan pola belajar peserta didik untuk lebih aktif dengan menemukan sendiri, menyelidiki sendiri, untuk mendapatkan hasil yang akan tidak mudah dilupakan dalam ingatan peserta didik” (Dinata dan Zainul, 2020:7).
+  </p>
+  <div class="solu_title">
+  <h3>Tahapan Model discovery learning dalam E-Modul</h3>
+</div>
+<table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Fase</th>
+      <th scope="col">Deskripsi</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td><i>Stimulation</i>
+      (Pemberian Rangsangan )</td>
+      <td>Peserta didik diberikan permasalahan diawal dengan mengamati salah satu pertanyaan pada materi trigonometri yang disajikan  di dalam e-modul.</td>
+    </tr>
+  </tbody>
+</table>
               </div>
             </div>
           </div>
