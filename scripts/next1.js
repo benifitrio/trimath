768d46064a338638faac6a3d43b8983a2928067a
bendafit <benidanuarifitrio@gmail.com>
2023-06-03 09:36:02 +0700

    first commit

--- scripts/next1.js
+++ scripts/next1.js
@@ -4,6 +4,12 @@ function next1Page() {
  <div class="section_our_solution">
  <button class="divco blue mb-2" href=""><span>PROBLEM STATEMENT ( Identifikasi Masalah)</span></button>
  <p>Setelah melakukan identifikasi, didapatkan jawaban sementara yaitu : </p>
+ <form class="mb-2">
+  <div class="form-group">
+    <label for="exampleFormControlTextarea1 " class="font-weight-bold">Jawab</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+</form>
  <div class="w-100 d-flex justify-content-between">
  <a href="#pert1" type="button" class="btn btn-success ">Sebelumnya</a>
  <a href="#next2" type="button" class="btn btn-success">Selanjutnya</a>
