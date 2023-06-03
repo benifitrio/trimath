c0a9daa1ff2f8427f64edc15e1ae252815fea7c1
bendafit <benidanuarifitrio@gmail.com>
2023-06-04 02:00:32 +0700

    first commit

--- scripts/next2.js
+++ scripts/next2.js
@@ -2,10 +2,16 @@ function next2Page() {
     judul.innerHTML = `<span class="h6">Pertemuan 1: Pengertian Trigonometri</span>`
     content.innerHTML = `
 <div class="section_our_solution">
-<button class="divco blue mb-2 mt-3" href=""><span>PROBLEM STATEMENT ( Identifikasi Masalah)</span></button>
-<p>Setelah melakukan identifikasi, didapatkan jawaban sementara yaitu : </p>
-<button class="divco blue mb-2" href=""><span>DATA COLLECTION ( Pengumpulan Data)</span></button>
+<ul>
+<li>Dalam perbandingan trigonometri rumus perbandingan trigonometri ada 3 yaitu sinus, cosinus dan tangen.</li>
+<ol>
+<li>
+<b>Sinus</b><br>
+Nilai Perbandingan ini mempunyai nama khusus yaitu Sinus (disingkat sin). Sin suatu sudut yang dapat ditentukan dengan membagi panjang sisi depan dan sisi miring segitiga. <br>
+<img src="../sinus.png" class="img-fluid " alt="Responsive image"> 
+</li>
 
+</ol>
 </ul>
 <div class="mt-4 w-100 d-flex justify-content-between">
 <a href="#next1" type="button" class="btn btn-success ">Sebelumnya</a>
