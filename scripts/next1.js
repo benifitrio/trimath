8e2f0975a1ded3ac9500633d01e3aa2090b8c0dd
bendafit <benidanuarifitrio@gmail.com>
2023-06-03 09:46:16 +0700

    first commit

--- scripts/next1.js
+++ scripts/next1.js
@@ -2,15 +2,19 @@ function next1Page() {
     judul.innerText = `Pertemuan 1: Pengertian Trigonometri`
     content.innerHTML = `
  <div class="section_our_solution">
- <button class="divco blue mb-2" href=""><span>PROBLEM STATEMENT ( Identifikasi Masalah)</span></button>
+ <button class="divco blue mb-2 mt-3" href=""><span>PROBLEM STATEMENT ( Identifikasi Masalah)</span></button>
  <p>Setelah melakukan identifikasi, didapatkan jawaban sementara yaitu : </p>
- <form class="mb-2">
+ <form class="mb-5">
   <div class="form-group">
-    <label for="exampleFormControlTextarea1 " class="font-weight-bold">Jawab</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+    <span  class="font-weight-bold">Jawab</span>
+    <textarea class="form-control" rows="3" placeholder="tulis jawabanmu disini...."></textarea>
   </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
 </form>
- <div class="w-100 d-flex justify-content-between">
+<button class="divco blue mb-2" href=""><span>DATA COLLECTION ( Pengumpulan Data)</span></button>
+ <p>Untuk membuktikan hipotesis diatas, Kalian dapat mengumpulkan beberapa data dan informasi sebagai berikut :  </p>
+ 
+ <div class="mt-4 w-100 d-flex justify-content-between">
  <a href="#pert1" type="button" class="btn btn-success ">Sebelumnya</a>
  <a href="#next2" type="button" class="btn btn-success">Selanjutnya</a>
  </div>
