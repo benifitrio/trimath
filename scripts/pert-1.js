98668ab5de8bc1da5975dc3026c30f86086e6d7e
bendafit <benidanuarifitrio@gmail.com>
2023-06-03 08:23:39 +0700

    first commit

--- scripts/pert-1.js
+++ scripts/pert-1.js
@@ -1,5 +1,5 @@
 function pert1Page() {
-    judul.innerText = `Pertemuan 1`
+    judul.innerText = `Pertemuan 1: Pengertian Trigonometri`
     content.innerHTML = `
     <div class="section_our_solution">
     <div class="my-4" id="testbutton_pert1">
