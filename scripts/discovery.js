dd27e836f2846ec3b29fbc8b3ec757e7fa8f4440
bendafit <benidanuarifitrio@gmail.com>
2023-06-03 07:45:05 +0700

    first commit

--- scripts/discovery.js
+++ scripts/discovery.js
@@ -1,6 +1,49 @@
 function discoveryPage() {
     judul.innerText = `Tahapan Discovery Learning`
-    content.innerHTML = `
-    halaman 1
- `
+    content.innerHTML =
+        `<div class="section_our_solution">
+          <div class="my-4 center">
+          <h5>Tahapan Model discovery learning</h5>
+        </div>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">Fase</th>
+              <th scope="col">Deskripsi</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><i>Stimulation</i>
+              (Pemberian Rangsangan)</td>
+              <td>Peserta didik diberikan permasalahan diawal dengan mengamati salah satu pertanyaan pada materi trigonometri yang disajikan  di dalam e-modul.</td>
+            </tr>
+            <tr>
+              <td><i>Problem Statement</i>
+              (Pernyataan/Identifikasi masalah)</td>
+              <td>Peserta didik dapat menuliskan apa saja pertanyaan untuk mengemukakan ide dalam identifikasi masalah dari hasil pengamatan untuk menyelesaikan masalah pada soal tersebut.</td>
+            </tr>
+            <tr>
+              <td><i>Data Collection</i>
+              (Pengumpulan Data)</td>
+              <td>Peserta didik diberikan kesempatan untuk menyelesaikan masalah berdasarkan identifikasi masalah.</td>
+            </tr>
+            <tr>
+              <td><i>Data Processing</i>
+              (Pengolahan Data)</td>
+              <td>Peserta didik dapat menuliskan proses pengolahan data dari hasil pengumpulan data untuk menyelesaikan masalah.</td>
+            </tr>
+            <tr>
+              <td><i>Verification</i>
+              (Pembuktian)</td>
+              <td>Peserta didik melakukan verifikasi dan pemeriksaan  hasil dari pengolahan data apakah sudah sesuai dengan pertanyaan yang ditunjukkan.</td>
+            </tr>
+            <tr>
+              <td><i>Generalization</i>
+              (Menarik Kesimpulan/Generalisasi)</td>
+              <td>Peserta didik dapat menyajikan hasil pekerjaannya berdasarkan pembuktian yang di dapat dari uraian masalah tersebut.</td>
+            </tr>
+          </tbody>
+        </table>
+                </div>`
 }
