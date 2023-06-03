88fb589662df124437736aba1c164c12351cfe15
bendafit <benidanuarifitrio@gmail.com>
2023-06-03 08:39:46 +0700

    first commit

--- scripts/pert-1.js
+++ scripts/pert-1.js
@@ -2,9 +2,9 @@ function pert1Page() {
     judul.innerText = `Pertemuan 1: Pengertian Trigonometri`
     content.innerHTML = `
     <div class="section_our_solution">
-    <div class="my-4" id="testbutton_pert1">
-   
-        </div>
+    <img src="../masalah1.png" class="img-fluid" alt="Responsive image">
+    <p>Dalam Kehidupan sehari – hari, seringkali kita menghadapi permasalahan yang melahirkan konsep matematika, sebagai contoh seseorang yang ingin mengukur tinggi sebuah pohon, menara, gedung bertingkat ataupun sesuatu yang memiliki ketinggian tertentu maka tidaklah mungkin secara fisik akan mengukur dari bawah ke atas (puncak) obyeknya dengan menggunakan meteran. Salah satu cabang matematika yang dapat dipakai dalam membantu pengukuran ini adalah “trigonometri”.</p>
+    <div class="my-4" id="testbutton_pert1"></div>
     </div>
 `
 }
