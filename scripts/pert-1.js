3aad5e5d8b8a3b130d37f8667ffcbf95cba1b6fb
bendafit <benidanuarifitrio@gmail.com>
2023-06-04 01:19:14 +0700

    first commit

--- scripts/pert-1.js
+++ scripts/pert-1.js
@@ -6,7 +6,7 @@ function pert1Page() {
     <p>Dalam Kehidupan sehari – hari, seringkali kita menghadapi permasalahan yang melahirkan konsep matematika, sebagai contoh seseorang yang ingin mengukur tinggi sebuah pohon, menara, gedung bertingkat ataupun sesuatu yang memiliki ketinggian tertentu maka tidaklah mungkin secara fisik akan mengukur dari bawah ke atas (puncak) obyeknya dengan menggunakan meteran. Salah satu cabang matematika yang dapat dipakai dalam membantu pengukuran ini adalah “trigonometri”.</p>
     <button class="divco blue mb-2" href=""><span>STIMULATION ( Pemberian Rangsangan)</span></button>
     <p style="text-indent:1em;">Pada gambar di atas kalian akan mengukur ketinggian pohon tersebut dengan menggunakan teorema phytagoras. Gambar di atas merupakan segitiga siku – siku dengan siku – siku di B. Segitiga di atas memiliki titik yaitu titik A,B dan C .dengan masing – masing sisi yaitu sisi d sisi e dan sisi f. </p>
-    <p style="text-indent:1em;">Berapakah tinggi pohon tersebut jika diketahui panjang AB = 3 m dan panjang AC = 5 m, dan tentukan nilai perbandingan pada sudut θ? </p>
+    <p style="text-indent:1em;">Berapakah tinggi pohon tersebut jika diketahui panjang AB = 3 m dan panjang AC = 5 m, dan tentukan nilai perbandingan pada sinus θ, cosinus θ, dan tangen θ? </p>
   
     <div class="w-100 d-flex justify-content-center">
     <a href="#next1" type="button" class="btn btn-success">Selanjutnya</a>
