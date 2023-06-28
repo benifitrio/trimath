efe133a8b71f177c2ff9b542629705b67f6ab541
bendafit <benidanuarifitrio@gmail.com>
2023-06-28 12:36:40 +0700

    first commit

--- scripts/next5.js
+++ scripts/next5.js
@@ -11,18 +11,6 @@ function next5Page() {
 Di Arab dan kebanyakan daerah muslim, trigonometri berkembang dengan pesat tidak saja karena alasan astronomi tetapi juga untuk kebutuhan ibadah. Seperti diketahui, orang muslim jika melakukan ibadah sholat, harus menghadap ke arah Qiblat, suatu bangunan di kota Mekkah. 
 </p>
 
-<div class="mb-3">
-<div class="form-group">
-<b>Jawab</b>
-<textarea class="form-control border border-warning" rows="3" placeholder="tulis jawabanmu disini...."></textarea>
-
-<button type="submit" class="btn btn-outline-primary">Submit</button>
-</div>
-</div>
-<button class="divco blue mb-2" href=""><span>DATA PROCESSING (Pengolahan Data)</span></button>
-<p style="text-indent:1em;" class="mb-0">Berdasarkan informasi yang didapat pada pengumpulan data maka, 
-permasalahan dapat diselesaikan dengan langkah – langkah berikut: </p>
-
 
 <div class="mt-4 w-100 d-flex justify-content-between">
 <a href="#next4" type="button" class="btn btn-success ">Sebelumnya</a>
