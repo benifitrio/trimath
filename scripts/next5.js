488757104700db601415aaa7ffdb81e87f25216b
bendafit <benidanuarifitrio@gmail.com>
2023-06-28 12:34:58 +0700

    first commit

--- scripts/next5.js
+++ scripts/next5.js
@@ -2,7 +2,15 @@ function next5Page() {
     judul.innerHTML = `<span class="h6 d-block center">Uraian Materi Perbandingan Trigonometri</span>`
     content.innerHTML = `
 <div class="section_our_solution">
-<p style="text-indent:1em;" class="mb-0">Setelah kalian mendapatkan beberapa data dan informasi apakah hipotesis kalian benar atau salah: </p>
+<div class="d-flex justify-content-around">
+<img src="../TUSI.png" class="img-fluid w-50" alt="Responsive image"> 
+<p style="text-indent:1em;" class="mb-0 mt-2">Studi tentang trigonometri sebagai cabang matematika, lepas dari astronomi pertama kali diberikan oleh Nashiruddin al-Tusi (1201-1274), lewat bukunya Treatise on the quadrilateral. Menurut OConners dan Robertson, mungkin ia pula yang pertama memperkenalkan Aturan Sinus (di bidang  datar). 
+</p>
+</div>
+<p style="text-indent:1em;" class="mb-0">
+Di Arab dan kebanyakan daerah muslim, trigonometri berkembang dengan pesat tidak saja karena alasan astronomi tetapi juga untuk kebutuhan ibadah. Seperti diketahui, orang muslim jika melakukan ibadah sholat, harus menghadap ke arah Qiblat, suatu bangunan di kota Mekkah. 
+</p>
+
 <div class="mb-3">
 <div class="form-group">
 <b>Jawab</b>
@@ -14,65 +22,11 @@ function next5Page() {
 <button class="divco blue mb-2" href=""><span>DATA PROCESSING (Pengolahan Data)</span></button>
 <p style="text-indent:1em;" class="mb-0">Berdasarkan informasi yang didapat pada pengumpulan data maka, 
 permasalahan dapat diselesaikan dengan langkah – langkah berikut: </p>
-<ul>
-<li>Langkah 1, menentukan tinggi pohon dengan menggunakan teorema phytagoras!</li>
-<div class="mb-3">
-<div class="form-group">
-<b>Jawab</b>
-<textarea class="form-control border border-warning" rows="3" placeholder="tulis jawabanmu disini...."></textarea>
-</div>
-<button type="submit" class="btn btn-outline-primary">Submit</button>
-</div>
-<li>Langkah 2, menentukan nilai perbandingan pada sudut θ menggunakan rumus perbandingan trigonometri (sinus, cosinus, tangen)!</li>
-<div class="mb-3">
-<div class="form-group">
-<b>Jawab</b>
-<textarea class="form-control border border-warning" rows="3" placeholder="tulis jawabanmu disini...."></textarea>
-</div>
-<button type="submit" class="btn btn-outline-primary">Submit</button>
-</div>
-</ul>
-<ul>
-<li>Langkah 1, menentukan tinggi pohon dengan menggunakan teorema phytagoras!</li>
-<div class="mb-3">
-<div class="form-group">
-<b>Jawab</b>
-<textarea class="form-control border border-warning" rows="3" placeholder="tulis jawabanmu disini...."></textarea>
-</div>
-<button type="submit" class="btn btn-outline-primary">Submit</button>
-</div>
-<li>Langkah 2, menentukan nilai perbandingan pada sudut θ menggunakan rumus perbandingan trigonometri (sinus, cosinus, tangen)!</li>
-<div class="mb-3">
-<div class="form-group">
-<b>Jawab</b>
-<textarea class="form-control border border-warning" rows="3" placeholder="tulis jawabanmu disini...."></textarea>
-</div>
-<button type="submit" class="btn btn-outline-primary">Submit</button>
-</div>
-</ul>
-<ul>
-<li>Langkah 1, menentukan tinggi pohon dengan menggunakan teorema phytagoras!</li>
-<div class="mb-3">
-<div class="form-group">
-<b>Jawab</b>
-<textarea class="form-control border border-warning" rows="3" placeholder="tulis jawabanmu disini...."></textarea>
-</div>
-<button type="submit" class="btn btn-outline-primary">Submit</button>
-</div>
-<li>Langkah 2, menentukan nilai perbandingan pada sudut θ menggunakan rumus perbandingan trigonometri (sinus, cosinus, tangen)!</li>
-<div class="mb-3">
-<div class="form-group">
-<b>Jawab</b>
-<textarea class="form-control border border-warning" rows="3" placeholder="tulis jawabanmu disini...."></textarea>
-</div>
-<button type="submit" class="btn btn-outline-primary">Submit</button>
-</div>
-</ul>
+
+
 <div class="mt-4 w-100 d-flex justify-content-between">
 <a href="#next4" type="button" class="btn btn-success ">Sebelumnya</a>
 <a href="#next6" type="button" class="btn btn-success">Selanjutnya</a>
 </div>
-
 </div>`
-
 }
