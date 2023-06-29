4676b72f98776702b233204f8558ed95e846db4a
bendafit <benidanuarifitrio@gmail.com>
2023-06-29 09:20:30 +0700

    first commit

--- scripts/pert-2.js
+++ scripts/pert-2.js
@@ -3,10 +3,11 @@ function pert2Page() {
     content.innerHTML = `
  <div class="section_our_solution">
  <img src="../trigonometri.png" class="img-fluid" alt="Responsive image">
- <p>Pada sub pertama, kalian telah menggunakan perbandingan trigonometri untuk mengukur tinggi objek yang besar tanpa harus mengukurnya secara langsung. pada bab kali ini kalian akan menentukan sudut istimewa dalam perbandingan trigonometri. Sudut istimewa dalam perbandingan trigonometri adalah sudut – sudut yang nilai perbandingannya dapat ditentukan secara eksak. Sudut istimewa akan sangat berguna dan banyak digunakan pada pembelajaran fisika. Sudut istimewa terhubung dengan beberapa kuadran.</p>
+ <p style="text-indent:1em;">Pada sub pertama, kalian telah menggunakan perbandingan trigonometri untuk mengukur tinggi objek yang besar tanpa harus mengukurnya secara langsung. pada bab kali ini kalian akan menentukan sudut istimewa dalam perbandingan trigonometri. Sudut istimewa dalam perbandingan trigonometri adalah sudut – sudut yang nilai perbandingannya dapat ditentukan secara eksak. Sudut istimewa akan sangat berguna dan banyak digunakan pada pembelajaran fisika. Sudut istimewa terhubung dengan beberapa kuadran.</p>
  <button class="divco blue mb-2" href=""><span>STIMULATION ( Pemberian Rangsangan)</span></button>
- <p style="text-indent:1em;">Pada gambar di atas kalian akan mengukur ketinggian pohon tersebut dengan menggunakan teorema phytagoras. Gambar di atas merupakan segitiga siku – siku dengan siku – siku di B. Segitiga di atas memiliki titik yaitu titik A,B dan C .dengan masing – masing sisi yaitu sisi d sisi e dan sisi f. </p>
- <p style="text-indent:1em;">Berapakah tinggi pohon tersebut jika diketahui panjang AB = 3 m dan panjang AC = 5 m, dan tentukan nilai perbandingan pada sinus θ, cosinus θ, dan tangen θ? </p>
+ <p style="text-indent:1em;">
+ Kalian akan melengkapi tabel berikut dengan nilai perbandingan trigonometrinya.
+ </p>
 
  <div class="w-100 d-flex justify-content-center">
  <a href="#next1" type="button" class="btn btn-success">Selanjutnya</a>
