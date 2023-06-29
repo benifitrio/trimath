04fd7ba31bfcd7a693b28075f7fe0750aaedd1a8
bendafit <benidanuarifitrio@gmail.com>
2023-06-29 09:18:21 +0700

    first commit

--- scripts/pert-2.js
+++ scripts/pert-2.js
@@ -2,8 +2,8 @@ function pert2Page() {
     judul.innerHTML = `<span class="h6">Sudut Istimewa Perbandingan Trigonometri</span>`
     content.innerHTML = `
  <div class="section_our_solution">
- <img src="../masalah1.png" class="img-fluid" alt="Responsive image">
- <p>Dalam Kehidupan sehari – hari, seringkali kita menghadapi permasalahan yang melahirkan konsep matematika, sebagai contoh seseorang yang ingin mengukur tinggi sebuah pohon, menara, gedung bertingkat ataupun sesuatu yang memiliki ketinggian tertentu maka tidaklah mungkin secara fisik akan mengukur dari bawah ke atas (puncak) obyeknya dengan menggunakan meteran. Salah satu cabang matematika yang dapat dipakai dalam membantu pengukuran ini adalah “trigonometri”.</p>
+ <img src="../trigonometri.png" class="img-fluid" alt="Responsive image">
+ <p>Pada sub pertama, kalian telah menggunakan perbandingan trigonometri untuk mengukur tinggi objek yang besar tanpa harus mengukurnya secara langsung. pada bab kali ini kalian akan menentukan sudut istimewa dalam perbandingan trigonometri. Sudut istimewa dalam perbandingan trigonometri adalah sudut – sudut yang nilai perbandingannya dapat ditentukan secara eksak. Sudut istimewa akan sangat berguna dan banyak digunakan pada pembelajaran fisika. Sudut istimewa terhubung dengan beberapa kuadran.</p>
  <button class="divco blue mb-2" href=""><span>STIMULATION ( Pemberian Rangsangan)</span></button>
  <p style="text-indent:1em;">Pada gambar di atas kalian akan mengukur ketinggian pohon tersebut dengan menggunakan teorema phytagoras. Gambar di atas merupakan segitiga siku – siku dengan siku – siku di B. Segitiga di atas memiliki titik yaitu titik A,B dan C .dengan masing – masing sisi yaitu sisi d sisi e dan sisi f. </p>
  <p style="text-indent:1em;">Berapakah tinggi pohon tersebut jika diketahui panjang AB = 3 m dan panjang AC = 5 m, dan tentukan nilai perbandingan pada sinus θ, cosinus θ, dan tangen θ? </p>
