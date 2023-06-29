8310f57381ebbfb472e04f6c35dfcef24048e69e
bendafit <benidanuarifitrio@gmail.com>
2023-06-29 10:58:38 +0700

    first commit

--- scripts/pert-2.js
+++ scripts/pert-2.js
@@ -4,7 +4,7 @@ function pert2Page() {
  <div class="section_our_solution">
  <img src="../trigonometri.png" class="img-fluid" alt="Responsive image">
  <p style="text-indent:1em;">Pada sub pertama, kalian telah menggunakan perbandingan trigonometri untuk mengukur tinggi objek yang besar tanpa harus mengukurnya secara langsung. pada bab kali ini kalian akan menentukan sudut istimewa dalam perbandingan trigonometri. Sudut istimewa dalam perbandingan trigonometri adalah sudut – sudut yang nilai perbandingannya dapat ditentukan secara eksak. Sudut istimewa akan sangat berguna dan banyak digunakan pada pembelajaran fisika. Sudut istimewa terhubung dengan beberapa kuadran.</p>
- <button class="divco blue mb-2" href=""><span>STIMULATION ( Pemberian Rangsangan)</span></button>
+ <button class="divco blue mb-2"><span>STIMULATION ( Pemberian Rangsangan)</span></button>
  <p style="text-indent:1em;">
  Kalian akan melengkapi tabel berikut dengan nilai perbandingan trigonometrinya.
  </p>
@@ -52,6 +52,7 @@ function pert2Page() {
   </tbody>
 </table>
  </div>
+ <button class="divco blue mb-2 mt-3"><span>PROBLEM STATEMENT ( Identifikasi Masalah)</span></button>
  <div class="w-100 d-flex justify-content-center mb-2">
  <a href="#next1" type="button" class="btn btn-success">Selanjutnya</a>
  </div>`
