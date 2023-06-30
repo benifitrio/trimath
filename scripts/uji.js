8d17250d78b811a160a23d206bb1beb1d044eca9
bendafit <benidanuarifitrio@gmail.com>
2023-06-30 10:09:56 +0700

    first commit

--- scripts/uji.js
+++ scripts/uji.js
@@ -3,40 +3,34 @@ function ujiPage() {
     content.innerHTML = `
 <div class="section_our_solution">
 <div id="quiz">
-<div class="question">
-    <p>1. Berapakah hasil dari 2 + 2?</p>
-    <div class="options list-group">
-        <label class="list-group-item"><input type="radio" name="q1" value="a">  1</label>
-        <label class="list-group-item"><input type="radio" name="q1" value="b"> 
-         3</label>
-        <label class="list-group-item"><input type="radio" name="q1" value="c"> 
-         4</label>
-        <label class="list-group-item"><input type="radio" name="q1" value="d"> 
-         5</label>
+        <div class="question">
+            <p>1. Berapakah hasil dari 2 + 2?</p>
+            <div class="options list-group">
+                <label class="list-group-item"><input type="radio" name="q1" value="a" >  1</label>
+                <label class="list-group-item"><input type="radio" name="q1" value="b" > 3</label>
+                <label class="list-group-item"><input type="radio" name="q1" value="c" "> 4</label>
+                <label class="list-group-item"><input type="radio" name="q1" value="d" > 5</label>
+            </div>
+        </div>
+        <div class="question">
+            <p>2. Berapakah hasil dari 5 x 6?</p>
+            <div class="options list-group">
+                <label class="list-group-item"><input type="radio" name="q2" value="a" > 10</label>
+                <label class="list-group-item"><input type="radio" name="q2" value="b" > 20</label>
+                <label class="list-group-item"><input type="radio" name="q2" value="c" > 30</label>
+                <label class="list-group-item"><input type="radio" name="q2" value="d" > 35</label>
+            </div>
+        </div>
+        <!-- Tambahkan pertanyaan lain di sini -->
+
+        <button id="selesai" class="btn btn-primary" onclick="submitQuiz()">Submit</button>
     </div>
-</div>
-<div class="question">
-    <p>2. Berapakah hasil dari 5 x 6?</p>
-    <div class="options list-group">
-        <label class="list-group-item"><input type="radio" name="q2" value="a"> 
-         10</label>
-        <label class="list-group-item"><input type="radio" name="q2" value="b"> 20</label>
-        <label class="list-group-item"><input type="radio" name="q2" value="c"> 
-         30</label>
-        <label class="list-group-item"><input type="radio" name="q2" value="d"> 
-         35</label>
+
+    <div id="result" class="result"></div>
+
+    <div id="restart" style="display: none;">
+        <button id="restart" class="btn btn-success" onclick="restartQuiz()">Ulangi Soal</button>
     </div>
-</div>
-<!-- Tambahkan pertanyaan lain di sini -->
-
-<button id="selesai" class="btn btn-primary">Submit</button>
-</div>
-
-<div id="result" class="result "></div>
-
-<div id="restart" style="display: none;">
-<button id="restart" class="btn btn-success">Ulangi Soal</button>
-</div>
 <div class="mt-4 w-100 d-flex justify-content-between mb-2">
 <a href="#pert1" type="button" class="btn btn-success ">Sebelumnya</a>
 <a href="#next2" type="button" class="btn btn-success">Selanjutnya</a>
@@ -44,6 +38,24 @@ function ujiPage() {
 </div>
 
 `
+        // Menambahkan event listener pada elemen "quiz" (elemen induk)
+    document.getElementById('quiz').addEventListener('click', function(event) {
+        // Mengecek apakah elemen yang diklik adalah elemen input radio
+        if (event.target.tagName === 'INPUT' && event.target.type === 'radio') {
+            // Mengambil elemen label terkait dengan input radio yang diklik
+            var label = event.target.parentNode;
+            console.log(event.target.tagName)
+                // Menghapus kelas "selected" dari semua elemen label pada pertanyaan yang sama
+            var question = label.parentNode.parentNode;
+            var labels = question.getElementsByClassName('list-group-item');
+            for (var i = 0; i < labels.length; i++) {
+                labels[i].classList.remove('selected');
+            }
+
+            // Menambahkan kelas "selected" pada elemen label yang diklik
+            label.classList.add('selected');
+        }
+    });
 
     function submitQuiz() {
         const answers = ['c', 'c']; // Jawaban yang benar untuk setiap pertanyaan (sesuaikan dengan pertanyaan yang ada)
@@ -62,6 +74,9 @@ function ujiPage() {
 
             if (selectedOption && selectedOption.value === answers[i]) {
                 score++;
+                selectedOption.parentNode.classList.add('correct');
+            } else if (selectedOption) {
+                selectedOption.parentNode.classList.add('incorrect');
             }
         }
 
@@ -83,13 +98,13 @@ function ujiPage() {
         quizContainer.style.display = 'block';
         restartContainer.style.display = 'none';
 
-        // Mengatur ulang pilihan jawaban
+        // Mengatur ulang pilihan jawaban dan warna latar
         const options = quizContainer.querySelectorAll('input[type="radio"]');
         options.forEach(option => {
             option.checked = false;
+            option.parentNode.classList.remove('correct', 'incorrect', 'selected');
         });
     }
-
-    document.querySelector("#selesai").addEventListener('click', submitQuiz)
-    document.querySelector("#restart").addEventListener('click', restartQuiz)
+    document.getElementById('selesai').addEventListener('click', submitQuiz);
+    document.getElementById('restart').addEventListener('click', restartQuiz);
 }
