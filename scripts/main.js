c9cf8e143b8e6c09ac6134261d3f7565300b7c72
bendafit <benidanuarifitrio@gmail.com>
2023-08-04 15:42:54 +0700

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,54 @@
-const arrows = document.querySelectorAll(".arrow");
-
-arrows.forEach((arrow) => {
-    arrow.addEventListener("click", (e) => {
-        const arrowParent = e.target.closest(".arrow").parentElement.parentElement;
-        arrowParent.classList.toggle("showMenu");
-    });
-});
-
+// Selecting the sidebar and buttons
 const sidebar = document.querySelector(".sidebar");
-const sidebarBtn = document.querySelector(".bx-menu");
+const sidebarOpenBtn = document.querySelector("#sidebar-open");
+const sidebarCloseBtn = document.querySelector("#sidebar-close");
+const sidebarLockBtn = document.querySelector("#lock-icon");
 
-sidebarBtn.addEventListener("click", () => {
+// Function to toggle the lock state of the sidebar
+const toggleLock = () => {
+    sidebar.classList.toggle("locked");
+    // If the sidebar is not locked
+    if (!sidebar.classList.contains("locked")) {
+        sidebar.classList.add("hoverable");
+        sidebarLockBtn.classList.replace("bx-lock-alt", "bx-lock-open-alt");
+    } else {
+        sidebar.classList.remove("hoverable");
+        sidebarLockBtn.classList.replace("bx-lock-open-alt", "bx-lock-alt");
+    }
+};
+
+// Function to hide the sidebar when the mouse leaves
+const hideSidebar = () => {
+    if (sidebar.classList.contains("hoverable")) {
+        sidebar.classList.add("close");
+    }
+};
+
+// Function to show the sidebar when the mouse enter
+const showSidebar = () => {
+    if (sidebar.classList.contains("hoverable")) {
+        sidebar.classList.remove("close");
+    }
+};
+
+// Function to show and hide the sidebar
+const toggleSidebar = () => {
     sidebar.classList.toggle("close");
-});
+};
 
+// If the window width is less than 800px, close the sidebar and remove hoverability and lock
+if (window.innerWidth < 800) {
+    sidebar.classList.add("close");
+    sidebar.classList.remove("locked");
+    sidebar.classList.remove("hoverable");
+}
+
+// Adding event listeners to buttons and sidebar for the corresponding actions
+sidebarLockBtn.addEventListener("click", toggleLock);
+sidebar.addEventListener("mouseleave", hideSidebar);
+sidebar.addEventListener("mouseenter", showSidebar);
+sidebarOpenBtn.addEventListener("click", toggleSidebar);
+sidebarCloseBtn.addEventListener("click", toggleSidebar);
 document.addEventListener("DOMContentLoaded", () => {
 
     // Load page content
