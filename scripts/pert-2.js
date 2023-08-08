d503397c56a22c38e1baea4527215e14f6808350
bendafit <benidanuarifitrio@gmail.com>
2023-08-08 15:31:17 +0700

    first commit

--- scripts/pert-2.js
+++ scripts/pert-2.js
@@ -9,48 +9,145 @@ function pert2Page() {
  Kalian akan melengkapi tabel berikut dengan nilai perbandingan trigonometrinya.
  </p>
  <div class="table-responsive">
- <table class="table table-success table-striped">
- <caption>Ket: ∞ tak terdefinisi</caption>
- <thead>
-    <tr>
-      <th scope="col" rowspan="2" style="text-align: center; vertical-align :middle">Fungsi Trigonometri</th>
-      <th scope="col" colspan="5" style="text-align: center ">Sudut</th>
-    </tr>
-    <tr>
-      <th scope="col">0</th>
-      <th scope="col">30</th>
-      <th scope="col">45</th>
-      <th scope="col">60</th>
-      <th scope="col">90</th>
-  </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">Sinus</th>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-    </tr>
-    <tr>
-      <th scope="row">Cosinus</th>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-    </tr>
-    <tr>
-      <th scope="row">Tangen</th>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-      <td>...</td>
-    </tr>
-  </tbody>
-</table>
+ 
+ <iframe
+ id="JotFormIFrame-232192388103453"
+ title="Online Quiz"
+ onload="window.parent.scrollTo(0,0)"
+ allowtransparency="true"
+ allowfullscreen="true"
+ allow="geolocation; microphone; camera"
+ src="https://form.jotform.com/232192388103453"
+ frameborder="0"
+ style="min-width:100%;max-width:100%;height:539px;border:none;"
+ scrolling="yes"
+>
+</iframe>
+<script type="text/javascript">
+var ifr = document.getElementById("JotFormIFrame-232192388103453");
+if (ifr) {
+ var src = ifr.src;
+ var iframeParams = [];
+ if (window.location.href && window.location.href.indexOf("?") > -1) {
+   iframeParams = iframeParams.concat(window.location.href.substr(window.location.href.indexOf("?") + 1).split('&'));
+ }
+ if (src && src.indexOf("?") > -1) {
+   iframeParams = iframeParams.concat(src.substr(src.indexOf("?") + 1).split("&"));
+   src = src.substr(0, src.indexOf("?"))
+ }
+ iframeParams.push("isIframeEmbed=1");
+ ifr.src = src + "?" + iframeParams.join('&');
+}
+window.handleIFrameMessage = function(e) {
+ if (typeof e.data === 'object') { return; }
+ var args = e.data.split(":");
+ if (args.length > 2) { iframe = document.getElementById("JotFormIFrame-" + args[(args.length - 1)]); } else { iframe = document.getElementById("JotFormIFrame"); }
+ if (!iframe) { return; }
+ switch (args[0]) {
+   case "scrollIntoView":
+     iframe.scrollIntoView();
+     break;
+   case "setHeight":
+     iframe.style.height = args[1] + "px";
+     if (!isNaN(args[1]) && parseInt(iframe.style.minHeight) > parseInt(args[1])) {
+       iframe.style.minHeight = args[1] + "px";
+     }
+     break;
+   case "collapseErrorPage":
+     if (iframe.clientHeight > window.innerHeight) {
+       iframe.style.height = window.innerHeight + "px";
+     }
+     break;
+   case "reloadPage":
+     window.location.reload();
+     break;
+   case "loadScript":
+     if( !window.isPermitted(e.origin, ['jotform.com', 'jotform.pro']) ) { break; }
+     var src = args[1];
+     if (args.length > 3) {
+         src = args[1] + ':' + args[2];
+     }
+     var script = document.createElement('script');
+     script.src = src;
+     script.type = 'text/javascript';
+     document.body.appendChild(script);
+     break;
+   case "exitFullscreen":
+     if      (window.document.exitFullscreen)        window.document.exitFullscreen();
+     else if (window.document.mozCancelFullScreen)   window.document.mozCancelFullScreen();
+     else if (window.document.mozCancelFullscreen)   window.document.mozCancelFullScreen();
+     else if (window.document.webkitExitFullscreen)  window.document.webkitExitFullscreen();
+     else if (window.document.msExitFullscreen)      window.document.msExitFullscreen();
+     break;
+ }
+ var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
+ if(isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
+   var urls = {"docurl":encodeURIComponent(document.URL),"referrer":encodeURIComponent(document.referrer)};
+   iframe.contentWindow.postMessage(JSON.stringify({"type":"urls","value":urls}), "*");
+ }
+};
+window.isPermitted = function(originUrl, whitelisted_domains) {
+ var url = document.createElement('a');
+ url.href = originUrl;
+ var hostname = url.hostname;
+ var result = false;
+ if( typeof hostname !== 'undefined' ) {
+   whitelisted_domains.forEach(function(element) {
+       if( hostname.slice((-1 * element.length - 1)) === '.'.concat(element) ||  hostname === element ) {
+           result = true;
+       }
+   });
+   return result;
+ }
+};
+if (window.addEventListener) {
+ window.addEventListener("message", handleIFrameMessage, false);
+} else if (window.attachEvent) {
+ window.attachEvent("onmessage", handleIFrameMessage);
+}
+</script>
+//  <table class="table table-success table-striped">
+//  <caption>Ket: ∞ tak terdefinisi</caption>
+//  <thead>
+//     <tr>
+//       <th scope="col" rowspan="2" style="text-align: center; vertical-align :middle">Fungsi Trigonometri</th>
+//       <th scope="col" colspan="5" style="text-align: center ">Sudut</th>
+//     </tr>
+//     <tr>
+//       <th scope="col">0</th>
+//       <th scope="col">30</th>
+//       <th scope="col">45</th>
+//       <th scope="col">60</th>
+//       <th scope="col">90</th>
+//   </tr>
+//   </thead>
+//   <tbody>
+//     <tr>
+//       <th scope="row">Sinus</th>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//     </tr>
+//     <tr>
+//       <th scope="row">Cosinus</th>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//     </tr>
+//     <tr>
+//       <th scope="row">Tangen</th>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//       <td>...</td>
+//     </tr>
+//   </tbody>
+// </table>
  </div>
  <button class="divco blue mb-2 mt-3"><span>PROBLEM STATEMENT ( Identifikasi Masalah)</span></button>
  <p style="text-indent:1em;">
