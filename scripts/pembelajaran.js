61185571bb70e9d8c4ddc853af7f4c280278e275
bendafit <benidanuarifitrio@gmail.com>
2023-08-29 19:11:20 +0700

    first commit

--- scripts/pembelajaran.js
+++ scripts/pembelajaran.js
@@ -11,10 +11,20 @@ function pembelajaranPage() {
     <ul class="list-group-flush">
     <li class="list-group-item">Stimulation : peserta didik diberikan apersepsi berupa pertanyaan yang disajikan didalam e-modul. Dalam hal ini peserta didik dapat menyelidiki masalah tersebut </li>
     <li class="list-group-item">Problem Statement : peserta didik diberikan pertanyaan peserta didik dapat menjawab pertanyaan tersebut yang kemudian dijadikan jawaban sementara (hipotesis) </li>
+    <li class="list-group-item">Data Collection : peserta didik dapat mengumpulkan berbagai Informasi dengan membaca materi yang tersedia pada e-modul</li>
+    <li class="list-group-item">Data Processing : peserta didik dapat mengolah data berdasarkan informasi yang di dapatkan dari pengumpulan data</li>
+    <li class="list-group-item">Verification : peserta didik dapat melakukan verification berdasarkan hasil data yang sudah ada sebelumnya dan dihubungkan dengan hasildata yang telah diperoleh.</li>
+    <li class="list-group-item">Generalization: peserta didik dapat menarik kesimpulan pembelajaran pada kolom yang telah disediakan.</li>
   </ul>
     </li>
     <li class="list-group-item">
-  
+    Dibagian akhir sub materi terdapat contoh soal yang tersedia melalui link youtube. Cobalah untuk di amati dan dipahami.
+    </li>
+    <li class="list-group-item">
+    Pada akhir kegiatan belajar terdapat soal uji kompetensi pilihan ganda dan essay cobalah untuk dikerjakan. 
+    </li>
+    <li class="list-group-item">
+    Jika kalian menemui kesulitan, kalian dapat bertanya dengan teman maupun guru mata pelajaran.    
     </li>
   </ol>
 
