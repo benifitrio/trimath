19f763dfb6ecfe912572123c4bc6aa8a26503df6
bendafit <benidanuarifitrio@gmail.com>
2023-09-03 13:50:32 +0700

    first commit

--- scripts/pert-2.js
+++ scripts/pert-2.js
@@ -21,20 +21,20 @@ function pert2Page() {
  Setelah melakukan identifikasi, didapatkan jawaban sementara yaitu: 
  </p>
  <iframe
- id="JotFormIFrame-232170612128446"
- title="Jawab"
+ id="JotFormIFrame-232450973377463"
+ title="Video Submit Form"
  onload="window.parent.scrollTo(0,0)"
  allowtransparency="true"
  allowfullscreen="true"
  allow="geolocation; microphone; camera"
- src="https://form.jotform.com/232170612128446"
+ src="https://form.jotform.com/232450973377463"
  frameborder="0"
  style="min-width:100%;max-width:100%;height:539px;border:none;"
  scrolling="yes"
 >
 </iframe>
 <script type="text/javascript">
-var ifr = document.getElementById("JotFormIFrame-232170612128446");
+var ifr = document.getElementById("JotFormIFrame-232450973377463");
 if (ifr) {
  var src = ifr.src;
  var iframeParams = [];
@@ -143,99 +143,99 @@ Untuk membuktikan hipotesis diatas, Kalian dapat mengumpulkan beberapa data dan 
   <p style="text-indent:1em;">Gambar di atas merupakan gambar segitiga sama sisi dengan besarnya masing-masing sudut adalah 60°. Pada ΔABC tersebut ditarik sebuah garis dari titik C menuju titik D yang tegak lurus dengan garis AB, sehingga membagi sudut ACB menjadi dua bagian yang besarnya sama yakni 30°. Misalkan panjang AD = BD = x, maka panjang AC = BC = 2x.</p>
   <p style="text-indent:1em;">Pada gambar di atas panjang CD belum diketahui, berapakah panjang CD Dengan menggunakan rumus teorema pythagoras  </p>
   <iframe
-id="JotFormIFrame-232170612128446"
-title="Jawab"
-onload="window.parent.scrollTo(0,0)"
-allowtransparency="true"
-allowfullscreen="true"
-allow="geolocation; microphone; camera"
-src="https://form.jotform.com/232170612128446"
-frameborder="0"
-style="min-width:100%;max-width:100%;height:539px;border:none;"
-scrolling="yes"
+ id="JotFormIFrame-232450973377463"
+ title="Video Submit Form"
+ onload="window.parent.scrollTo(0,0)"
+ allowtransparency="true"
+ allowfullscreen="true"
+ allow="geolocation; microphone; camera"
+ src="https://form.jotform.com/232450973377463"
+ frameborder="0"
+ style="min-width:100%;max-width:100%;height:539px;border:none;"
+ scrolling="yes"
 >
 </iframe>
 <script type="text/javascript">
-var ifr = document.getElementById("JotFormIFrame-232170612128446");
+var ifr = document.getElementById("JotFormIFrame-232450973377463");
 if (ifr) {
-var src = ifr.src;
-var iframeParams = [];
-if (window.location.href && window.location.href.indexOf("?") > -1) {
-  iframeParams = iframeParams.concat(window.location.href.substr(window.location.href.indexOf("?") + 1).split('&'));
-}
-if (src && src.indexOf("?") > -1) {
-  iframeParams = iframeParams.concat(src.substr(src.indexOf("?") + 1).split("&"));
-  src = src.substr(0, src.indexOf("?"))
-}
-iframeParams.push("isIframeEmbed=1");
-ifr.src = src + "?" + iframeParams.join('&');
+ var src = ifr.src;
+ var iframeParams = [];
+ if (window.location.href && window.location.href.indexOf("?") > -1) {
+   iframeParams = iframeParams.concat(window.location.href.substr(window.location.href.indexOf("?") + 1).split('&'));
+ }
+ if (src && src.indexOf("?") > -1) {
+   iframeParams = iframeParams.concat(src.substr(src.indexOf("?") + 1).split("&"));
+   src = src.substr(0, src.indexOf("?"))
+ }
+ iframeParams.push("isIframeEmbed=1");
+ ifr.src = src + "?" + iframeParams.join('&');
 }
 window.handleIFrameMessage = function(e) {
-if (typeof e.data === 'object') { return; }
-var args = e.data.split(":");
-if (args.length > 2) { iframe = document.getElementById("JotFormIFrame-" + args[(args.length - 1)]); } else { iframe = document.getElementById("JotFormIFrame"); }
-if (!iframe) { return; }
-switch (args[0]) {
-  case "scrollIntoView":
-    iframe.scrollIntoView();
-    break;
-  case "setHeight":
-    iframe.style.height = args[1] + "px";
-    if (!isNaN(args[1]) && parseInt(iframe.style.minHeight) > parseInt(args[1])) {
-      iframe.style.minHeight = args[1] + "px";
-    }
-    break;
-  case "collapseErrorPage":
-    if (iframe.clientHeight > window.innerHeight) {
-      iframe.style.height = window.innerHeight + "px";
-    }
-    break;
-  case "reloadPage":
-    window.location.reload();
-    break;
-  case "loadScript":
-    if( !window.isPermitted(e.origin, ['jotform.com', 'jotform.pro']) ) { break; }
-    var src = args[1];
-    if (args.length > 3) {
-        src = args[1] + ':' + args[2];
-    }
-    var script = document.createElement('script');
-    script.src = src;
-    script.type = 'text/javascript';
-    document.body.appendChild(script);
-    break;
-  case "exitFullscreen":
-    if      (window.document.exitFullscreen)        window.document.exitFullscreen();
-    else if (window.document.mozCancelFullScreen)   window.document.mozCancelFullScreen();
-    else if (window.document.mozCancelFullscreen)   window.document.mozCancelFullScreen();
-    else if (window.document.webkitExitFullscreen)  window.document.webkitExitFullscreen();
-    else if (window.document.msExitFullscreen)      window.document.msExitFullscreen();
-    break;
-}
-var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
-if(isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
-  var urls = {"docurl":encodeURIComponent(document.URL),"referrer":encodeURIComponent(document.referrer)};
-  iframe.contentWindow.postMessage(JSON.stringify({"type":"urls","value":urls}), "*");
-}
+ if (typeof e.data === 'object') { return; }
+ var args = e.data.split(":");
+ if (args.length > 2) { iframe = document.getElementById("JotFormIFrame-" + args[(args.length - 1)]); } else { iframe = document.getElementById("JotFormIFrame"); }
+ if (!iframe) { return; }
+ switch (args[0]) {
+   case "scrollIntoView":
+     iframe.scrollIntoView();
+     break;
+   case "setHeight":
+     iframe.style.height = args[1] + "px";
+     if (!isNaN(args[1]) && parseInt(iframe.style.minHeight) > parseInt(args[1])) {
+       iframe.style.minHeight = args[1] + "px";
+     }
+     break;
+   case "collapseErrorPage":
+     if (iframe.clientHeight > window.innerHeight) {
+       iframe.style.height = window.innerHeight + "px";
+     }
+     break;
+   case "reloadPage":
+     window.location.reload();
+     break;
+   case "loadScript":
+     if( !window.isPermitted(e.origin, ['jotform.com', 'jotform.pro']) ) { break; }
+     var src = args[1];
+     if (args.length > 3) {
+         src = args[1] + ':' + args[2];
+     }
+     var script = document.createElement('script');
+     script.src = src;
+     script.type = 'text/javascript';
+     document.body.appendChild(script);
+     break;
+   case "exitFullscreen":
+     if      (window.document.exitFullscreen)        window.document.exitFullscreen();
+     else if (window.document.mozCancelFullScreen)   window.document.mozCancelFullScreen();
+     else if (window.document.mozCancelFullscreen)   window.document.mozCancelFullScreen();
+     else if (window.document.webkitExitFullscreen)  window.document.webkitExitFullscreen();
+     else if (window.document.msExitFullscreen)      window.document.msExitFullscreen();
+     break;
+ }
+ var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
+ if(isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
+   var urls = {"docurl":encodeURIComponent(document.URL),"referrer":encodeURIComponent(document.referrer)};
+   iframe.contentWindow.postMessage(JSON.stringify({"type":"urls","value":urls}), "*");
+ }
 };
 window.isPermitted = function(originUrl, whitelisted_domains) {
-var url = document.createElement('a');
-url.href = originUrl;
-var hostname = url.hostname;
-var result = false;
-if( typeof hostname !== 'undefined' ) {
-  whitelisted_domains.forEach(function(element) {
-      if( hostname.slice((-1 * element.length - 1)) === '.'.concat(element) ||  hostname === element ) {
-          result = true;
-      }
-  });
-  return result;
-}
+ var url = document.createElement('a');
+ url.href = originUrl;
+ var hostname = url.hostname;
+ var result = false;
+ if( typeof hostname !== 'undefined' ) {
+   whitelisted_domains.forEach(function(element) {
+       if( hostname.slice((-1 * element.length - 1)) === '.'.concat(element) ||  hostname === element ) {
+           result = true;
+       }
+   });
+   return result;
+ }
 };
 if (window.addEventListener) {
-window.addEventListener("message", handleIFrameMessage, false);
+ window.addEventListener("message", handleIFrameMessage, false);
 } else if (window.attachEvent) {
-window.attachEvent("onmessage", handleIFrameMessage);
+ window.attachEvent("onmessage", handleIFrameMessage);
 }
 </script>
 <p style="text-indent:1em;">Langkah 2 setelah didapatkan hasil pada panjang CD kalian dapat mencari nilai pada sudut 30°, dan 60° dengan menggunakan rumus perbandingan trigonometri sinus cosinus dan tangen</p>
@@ -247,99 +247,99 @@ window.attachEvent("onmessage", handleIFrameMessage);
   <p style="text-indent:1em;">Perhatikan segitiga ABC di atas dengan siku-siku berada di titik A. sudut B =  C = 45°. Jika panjang AB = AC = x </p>
   <p style="text-indent:1em;">Pada gambar di atas panjang BC belum diketahui, berapakah panjang BC Dengan menggunakan rumus teorema Pythagoras</p>
   <iframe
-id="JotFormIFrame-232170612128446"
-title="Jawab"
-onload="window.parent.scrollTo(0,0)"
-allowtransparency="true"
-allowfullscreen="true"
-allow="geolocation; microphone; camera"
-src="https://form.jotform.com/232170612128446"
-frameborder="0"
-style="min-width:100%;max-width:100%;height:539px;border:none;"
-scrolling="yes"
+ id="JotFormIFrame-232450973377463"
+ title="Video Submit Form"
+ onload="window.parent.scrollTo(0,0)"
+ allowtransparency="true"
+ allowfullscreen="true"
+ allow="geolocation; microphone; camera"
+ src="https://form.jotform.com/232450973377463"
+ frameborder="0"
+ style="min-width:100%;max-width:100%;height:539px;border:none;"
+ scrolling="yes"
 >
 </iframe>
 <script type="text/javascript">
-var ifr = document.getElementById("JotFormIFrame-232170612128446");
+var ifr = document.getElementById("JotFormIFrame-232450973377463");
 if (ifr) {
-var src = ifr.src;
-var iframeParams = [];
-if (window.location.href && window.location.href.indexOf("?") > -1) {
-  iframeParams = iframeParams.concat(window.location.href.substr(window.location.href.indexOf("?") + 1).split('&'));
-}
-if (src && src.indexOf("?") > -1) {
-  iframeParams = iframeParams.concat(src.substr(src.indexOf("?") + 1).split("&"));
-  src = src.substr(0, src.indexOf("?"))
-}
-iframeParams.push("isIframeEmbed=1");
-ifr.src = src + "?" + iframeParams.join('&');
+ var src = ifr.src;
+ var iframeParams = [];
+ if (window.location.href && window.location.href.indexOf("?") > -1) {
+   iframeParams = iframeParams.concat(window.location.href.substr(window.location.href.indexOf("?") + 1).split('&'));
+ }
+ if (src && src.indexOf("?") > -1) {
+   iframeParams = iframeParams.concat(src.substr(src.indexOf("?") + 1).split("&"));
+   src = src.substr(0, src.indexOf("?"))
+ }
+ iframeParams.push("isIframeEmbed=1");
+ ifr.src = src + "?" + iframeParams.join('&');
 }
 window.handleIFrameMessage = function(e) {
-if (typeof e.data === 'object') { return; }
-var args = e.data.split(":");
-if (args.length > 2) { iframe = document.getElementById("JotFormIFrame-" + args[(args.length - 1)]); } else { iframe = document.getElementById("JotFormIFrame"); }
-if (!iframe) { return; }
-switch (args[0]) {
-  case "scrollIntoView":
-    iframe.scrollIntoView();
-    break;
-  case "setHeight":
-    iframe.style.height = args[1] + "px";
-    if (!isNaN(args[1]) && parseInt(iframe.style.minHeight) > parseInt(args[1])) {
-      iframe.style.minHeight = args[1] + "px";
-    }
-    break;
-  case "collapseErrorPage":
-    if (iframe.clientHeight > window.innerHeight) {
-      iframe.style.height = window.innerHeight + "px";
-    }
-    break;
-  case "reloadPage":
-    window.location.reload();
-    break;
-  case "loadScript":
-    if( !window.isPermitted(e.origin, ['jotform.com', 'jotform.pro']) ) { break; }
-    var src = args[1];
-    if (args.length > 3) {
-        src = args[1] + ':' + args[2];
-    }
-    var script = document.createElement('script');
-    script.src = src;
-    script.type = 'text/javascript';
-    document.body.appendChild(script);
-    break;
-  case "exitFullscreen":
-    if      (window.document.exitFullscreen)        window.document.exitFullscreen();
-    else if (window.document.mozCancelFullScreen)   window.document.mozCancelFullScreen();
-    else if (window.document.mozCancelFullscreen)   window.document.mozCancelFullScreen();
-    else if (window.document.webkitExitFullscreen)  window.document.webkitExitFullscreen();
-    else if (window.document.msExitFullscreen)      window.document.msExitFullscreen();
-    break;
-}
-var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
-if(isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
-  var urls = {"docurl":encodeURIComponent(document.URL),"referrer":encodeURIComponent(document.referrer)};
-  iframe.contentWindow.postMessage(JSON.stringify({"type":"urls","value":urls}), "*");
-}
+ if (typeof e.data === 'object') { return; }
+ var args = e.data.split(":");
+ if (args.length > 2) { iframe = document.getElementById("JotFormIFrame-" + args[(args.length - 1)]); } else { iframe = document.getElementById("JotFormIFrame"); }
+ if (!iframe) { return; }
+ switch (args[0]) {
+   case "scrollIntoView":
+     iframe.scrollIntoView();
+     break;
+   case "setHeight":
+     iframe.style.height = args[1] + "px";
+     if (!isNaN(args[1]) && parseInt(iframe.style.minHeight) > parseInt(args[1])) {
+       iframe.style.minHeight = args[1] + "px";
+     }
+     break;
+   case "collapseErrorPage":
+     if (iframe.clientHeight > window.innerHeight) {
+       iframe.style.height = window.innerHeight + "px";
+     }
+     break;
+   case "reloadPage":
+     window.location.reload();
+     break;
+   case "loadScript":
+     if( !window.isPermitted(e.origin, ['jotform.com', 'jotform.pro']) ) { break; }
+     var src = args[1];
+     if (args.length > 3) {
+         src = args[1] + ':' + args[2];
+     }
+     var script = document.createElement('script');
+     script.src = src;
+     script.type = 'text/javascript';
+     document.body.appendChild(script);
+     break;
+   case "exitFullscreen":
+     if      (window.document.exitFullscreen)        window.document.exitFullscreen();
+     else if (window.document.mozCancelFullScreen)   window.document.mozCancelFullScreen();
+     else if (window.document.mozCancelFullscreen)   window.document.mozCancelFullScreen();
+     else if (window.document.webkitExitFullscreen)  window.document.webkitExitFullscreen();
+     else if (window.document.msExitFullscreen)      window.document.msExitFullscreen();
+     break;
+ }
+ var isJotForm = (e.origin.indexOf("jotform") > -1) ? true : false;
+ if(isJotForm && "contentWindow" in iframe && "postMessage" in iframe.contentWindow) {
+   var urls = {"docurl":encodeURIComponent(document.URL),"referrer":encodeURIComponent(document.referrer)};
+   iframe.contentWindow.postMessage(JSON.stringify({"type":"urls","value":urls}), "*");
+ }
 };
 window.isPermitted = function(originUrl, whitelisted_domains) {
-var url = document.createElement('a');
-url.href = originUrl;
-var hostname = url.hostname;
-var result = false;
-if( typeof hostname !== 'undefined' ) {
-  whitelisted_domains.forEach(function(element) {
-      if( hostname.slice((-1 * element.length - 1)) === '.'.concat(element) ||  hostname === element ) {
-          result = true;
-      }
-  });
-  return result;
-}
+ var url = document.createElement('a');
+ url.href = originUrl;
+ var hostname = url.hostname;
+ var result = false;
+ if( typeof hostname !== 'undefined' ) {
+   whitelisted_domains.forEach(function(element) {
+       if( hostname.slice((-1 * element.length - 1)) === '.'.concat(element) ||  hostname === element ) {
+           result = true;
+       }
+   });
+   return result;
+ }
 };
 if (window.addEventListener) {
-window.addEventListener("message", handleIFrameMessage, false);
+ window.addEventListener("message", handleIFrameMessage, false);
 } else if (window.attachEvent) {
-window.attachEvent("onmessage", handleIFrameMessage);
+ window.attachEvent("onmessage", handleIFrameMessage);
 }
 </script>
 <p style="text-indent:1em;">Langkah 2 setelah didapatkan hasil pada panjang BC kalian dapat mencari nilai pada sudut 45° dengan menggunakan rumus perbandingan pada sinus cosinus dan tangen</p>
