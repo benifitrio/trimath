99c3c449479f4d10d24659d6c1bccb5460370d8c
bendafit <benidanuarifitrio@gmail.com>
2023-09-03 08:00:38 +0700

    first commit

--- scripts/pert-1.js
+++ scripts/pert-1.js
@@ -5,12 +5,20 @@ function pert1Page() {
     <img src="../images/masalah1.png" class="img-fluid" alt="Responsive image">
     <p style="text-indent:1em;">Dalam Kehidupan sehari – hari, seringkali kita menghadapi permasalahan yang melahirkan konsep matematika, sebagai contoh seseorang yang ingin mengukur tinggi sebuah pohon, menara, gedung bertingkat ataupun sesuatu yang memiliki ketinggian tertentu maka tidaklah mungkin secara fisik akan mengukur dari bawah ke atas (puncak) obyeknya dengan menggunakan meteran. Salah satu cabang matematika yang dapat dipakai dalam membantu pengukuran ini adalah “trigonometri”.</p>
     <p style="text-indent:1em;">Trigonometri berasal dari Bahasa yunani, trigonon artinya tiga sudut, dan metro artinya mengukur. Adapun rumus sinus, cosinus juga tangen diformulasikan oleh Surya siddhanta, ilmuwan india yang dipercaya hidup sekita abad 3SM. Selebihnya teori tentang Trigonometri disempurnakan oleh ilmuwan – ilmuwan lain di zaman berikutnya.</p>
+    <p style="text-indent:1em;">Trigonometri berasal dari Bahasa yunani, trigonon artinya tiga sudut, dan metro artinya mengukur. Adapun rumus sinus, cosinus juga tangen diformulasikan oleh Surya siddhanta, ilmuwan india yang dipercaya hidup sekita abad 3SM. Selebihnya teori tentang Trigonometri disempurnakan oleh ilmuwan – ilmuwan lain di zaman berikutnya.</p>
+    <p style="text-indent:1em;">Pada peradaban kehidupan budaya  Dayak, kajian mengenai Trigonometri sudah tercermin dari berbagai ikon kehidupan mereka. Misalnya para arsitektur sudah menerapkan kesetimbangan bangunan pada rumah adat  yang mereka ciptakan.</p>
+    <div class="flex justify-content-center">
+    <img src="../images/rumahadat.png" class="img-fluid" alt="Responsive image">
+    </div>
+    <p style="text-indent:1em;">Rumah adat tersebut berdiri kokoh sebagai hasil hubungan yang tepat antara besar sudut yang dikaitkan dengan panjang sisi –sisinya. Apakah para arsitektur tersebut mempelajari trigonometri juga? Pada sub bab ini akan dipahami konsep perbandingan trigonometri pada suatu segitiga siku – siku. Coba pahami deskripsi soal berikut.</p>
+
     <button class="divco blue mb-2" href="">
     <span>STIMULATION ( Pemberian Rangsangan)</span>
     </button>
     <p style="text-indent:1em;">
-    Pada gambar di atas kalian akan mengukur ketinggian pohon tersebut dengan menggunakan teorema phytagoras. Gambar di atas merupakan segitiga siku – siku dengan siku – siku di B. Segitiga di atas memiliki titik yaitu titik A,B dan C dengan masing – masing sisi yaitu sisi d sisi e dan sisi f. 
-    </p>
+    Pak yahya adalah seorang penjaga sekolah . Tinggi pak yahya adalah 1,6m. Dia mempunyai seorang anak namanya dani. Dani masih kelas II SD tinggi dani 1,2m. Dani adalah anak yang baik dan suka bertanya, dia pernah bertanya kepada ayahnya tentang tinggi tiang bendera di lapangan. Ayahnya pun menjawab 8m. Suatu sore dani menemani ayahnya membersihkan rumput liar dilapangan. Dani melihat bayangan setiap benda disana. Dani mengambil tali meteran dan mengukur panjang bayangan ayahnya dan panjang bayangan tiang bendera, yaitu 3m dan 15 m. Tetapi dani tidak dapat mengukur panjang bayangannya sendiri karena bayanganya mengikuti pergerakannya. Jika kamu sebagai dani, dapatkah kamu mengukur bayangan kamu sendiri dan menentukan nilai perbandingan untuk sinus, cosinus dan tangen?</p>
+    <p style="text-indent:1em;">
+    Pada gambar di atas kalian akan mengukur ketinggian pohon tersebut dengan menggunakan teorema phytagoras. Gambar di atas merupakan segitiga siku – siku dengan siku – siku di B. Segitiga di atas memiliki titik yaitu titik A,B dan C dengan masing – masing sisi yaitu sisi d sisi e dan sisi f.</p>
     <p style="text-indent:1em;">
     Berapakah tinggi pohon tersebut jika diketahui panjang AB = 3 m dan panjang AC = 5 m, dan tentukan nilai perbandingan pada sinus θ, cosinus θ, dan tangen θ?
     </p>
