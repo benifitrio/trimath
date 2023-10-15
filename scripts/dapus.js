284571740f0abc8e5436b027e8d180b621614305
bendafit <benidanuarifitrio@gmail.com>
2023-10-15 21:26:25 +0700

    first commit

--- scripts/dapus.js
+++ scripts/dapus.js
@@ -5,6 +5,7 @@ function dapusPage() {
         <ul class="list-group">
         <li class="list-group-item">Miyanto., Ngapiningsih dan Suparno. 2017. Matematika SMA/MA/SMK/MAK Kelas X. Klaten: Intan Pariwara.</li>
         <li class="list-group-item">Noormandiri. 2007. Matematika Untuk SMA Kelas X. Jakarta: Erlangga.</li>
+        <li class="list-group-item">Normandiri. 2021 . Matematika untuk SMA/MA kelas X. Jakarta: Erlangga.</li>
         <li class="list-group-item">Sudarmanta., E. 2016. Siaga Ujian Nasional Matematika untuk SMP/MTS. Klaten: CV Grafika Dua Tujuh.</li>
         <li class="list-group-item">Susanto., D.2021. Perbandingan Trigonometri. Jakarta: Kementerian Pendidikan, Kebudayaan, Riset, dan Teknologi Republik Indonesia.</li>
         <li class="list-group-item">Sutisna., E. 2020. Trigonometri Matematika Umum Kelas X. Jakarta: Kementerian Pendidikan dan Kebudayaan, Direktorat Jenderal Pendidikan Anak Usia Dini.</li>
