7def1bb342c21995de22f8a8f1584e7fdca9e96e
bendafit <benidanuarifitrio@gmail.com>
2023-10-15 21:28:22 +0700

    first commit

--- scripts/dapus.js
+++ scripts/dapus.js
@@ -1,7 +1,7 @@
 function dapusPage() {
     judul.innerText = `DAFTAR PUSTAKA`
     content.innerHTML =
-        `<div class="section_our_solution">
+        `<div class="section_our_solution justifytext">
         <ul class="list-group">
         <li class="list-group-item">Miyanto., Ngapiningsih dan Suparno. 2017. Matematika SMA/MA/SMK/MAK Kelas X. Klaten: Intan Pariwara.</li>
         <li class="list-group-item">Noormandiri. 2007. Matematika Untuk SMA Kelas X. Jakarta: Erlangga.</li>
