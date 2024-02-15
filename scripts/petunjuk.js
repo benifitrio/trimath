0168226d18b43307ee7d91bc0d897e7422c8751e
bendafit <benidanuarifitrio@gmail.com>
2024-02-15 10:15:26 +0700

    first commit

--- scripts/petunjuk.js
+++ scripts/petunjuk.js
@@ -2,8 +2,9 @@ function petunjukPage() {
     judul.innerText = `Petunjuk E-Modul`
     content.innerHTML = `<div class="section_our_solution justifytext">
     <ul class="list-group">
-        <li class="list-group-item">Miyanto., Ngapiningsih dan Suparno. 2017. Matematika SMA/MA/SMK/MAK Kelas X. Klaten: Intan Pariwara.</li>
-        <li class="list-group-item">Noormandiri. 2007. Matematika Untuk SMA Kelas X. Jakarta: Erlangga.</li>
+        <li class="list-group-item">Untuk menampilkan menu-menu pada e-modul, Anda dapat 'klik' (≡) pada pojok kiri atas.</li>
+        <li class="list-group-item">Cara mengunggah hasil jawaban yang sudah dikerjakan di buku, Anda dapat mengisi nama terlebih dahulu selanjutnya mengunggah jawaban dengan meng-klik kolom yang disediakan dan terakhir untuk menyimpan jawaban Anda diharuskan
+        untuk klik submit.</li>
       </ul>
 </div>`
 }
